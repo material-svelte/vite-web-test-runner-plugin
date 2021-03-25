23e95c1035302439a9ec31c128c1fb4d242e7bca
betaboon <betaboon@0x80.ninja>
2021-03-25 17:34:21 +0100

    add vue example

---
+++ examples/vue/src/App.test.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/vue';
+import { expect } from 'chai';
+import App from './App.vue';
+
+describe('<App>', () => {
+  it('renders vue documentation link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/Vue 3 Documentation/i);
+    expect(document.body.contains(linkElement));
+  });
+});
