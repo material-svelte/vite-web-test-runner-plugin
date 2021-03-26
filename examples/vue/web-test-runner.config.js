13a696de5033289f5ae9f2d65c2dc6aefb5c7a20
Haoqun Jiang <haoqunjiang@gmail.com>
2021-03-26 16:38:15 +0800

    fix: make the vue example work

--- examples/vue/web-test-runner.config.js
+++ examples/vue/web-test-runner.config.js
@@ -3,4 +3,16 @@ module.exports = {
   plugins: [
     vite(),
   ],
+  testRunnerHtml: testFramework => `
+    <html>
+      <head>
+        <script>
+          // Note: globals expected by @testing-library/vue
+          global = window;
+          process = { env: {} };
+        </script>
+        <script type="module" src="${testFramework}"></script>
+      </head>
+    </html>
+  `,
 };
