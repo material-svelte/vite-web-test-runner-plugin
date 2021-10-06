d5c15a82051b3b012154f0f055a7a0edfed9140c
Adam Laughlin <adam.laughlin@gmail.com>
2021-10-06 11:18:07 -0600

    add conditional for missing code

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const {isTestFilePath} = require('@web/test-runner')
 module.exports = function () {
   /** @type { import('vite').ViteDevServer } */
   let server
+
   /** @type { (url: string) => boolean } */
   const isTestRunnerFile = url => url.startsWith('/__web-dev-server') || url.startsWith('/__web-test-runner')
-  
-  return {
+
+  /** @type {import('@web/test-runner').TestRunnerPlugin } */
+  const plugin = {
     name: 'vite-plugin',
     async serverStart({app}) {
       server = await vite.createServer({clearScreen: false})
@@ -17,9 +19,10 @@ module.exports = function () {
     async serverStop() {
       await server.close()
     },
-    async serve({request, response, app, originalUrl, req, res, socket}) {
+    async serve({request}) {
       if (isTestRunnerFile(request.url)) return
-      return {body: (await server.transformRequest(request.url)).code}
+      const code = await server.transformRequest(request.path).code
+      return typeof code === 'string' ? {body: code} : undefined
     },
     transformImport({source}) {
       if (!isTestFilePath(source) || isTestRunnerFile(source)) return
@@ -27,4 +30,5 @@ module.exports = function () {
       return `${https ? 'https' : 'http'}://${host ?? 'localhost'}:${port ?? 80}${source}`
     },
   }
+  return plugin
 };
